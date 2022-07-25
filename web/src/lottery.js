563fa820c0b5ab3b974e079b2f5537b4ff6109db
ThisGuyIsNotAJumpingBear <rywang0037@hotmail.com>
2022-07-24 23:49:28 -0400

    add web

---
+++ web/src/lottery.js
@@ -0,0 +1,58 @@
+import web3 from "./web3";
+
+const address = '0xA6555B03f19eA58A8a4765F8862D8DF8AEDCd823';
+const abi = [
+    {
+      constant: true,
+      inputs: [],
+      name: 'manager',
+      outputs: [{ name: '', type: 'address' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'pickWinner',
+      outputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'getPlayers',
+      outputs: [{ name: '', type: 'address[]' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      constant: false,
+      inputs: [],
+      name: 'enter',
+      outputs: [],
+      payable: true,
+      stateMutability: 'payable',
+      type: 'function',
+    },
+    {
+      constant: true,
+      inputs: [{ name: '', type: 'uint256' }],
+      name: 'players',
+      outputs: [{ name: '', type: 'address' }],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      payable: false,
+      stateMutability: 'nonpayable',
+      type: 'constructor',
+    },
+  ];
+
+export default new web3.eth.Contract(abi, address);
